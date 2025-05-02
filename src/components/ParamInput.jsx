5f408d0fbd998ca3d0568b0a2a49f3d1c938b603
Skillovik <arambadasyan93@gmail.com>
2025-05-02 23:12:32 +0400

    opt_v1

--- src/components/ParamInput.jsx
+++ src/components/ParamInput.jsx
@@ -1,8 +1,6 @@
 import { RiTodoFill } from "react-icons/ri";
 import styles from "./Styles/MyProgress.module.css";
-import { useRef, useState } from "react";
-import { confirmAlert } from "react-confirm-alert"; // Import
-import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
+import { useState } from "react";
 import Swal from "sweetalert2";
 
 const ParamInput = ({
@@ -14,71 +12,34 @@ const ParamInput = ({
   inputDateVal,
 }) => {
   const [text, setText] = useState("");
-  const [actionHide, setactionHide] = useState(false);
 
-  const dialogRef = useRef(null);
-
-  function toggleDialog() {
-    if (dialogRef.current) {
-      return;
-    }
-    dialogRef.current.hasAttribute("open")
-      ? dialogRef.current.close()
-      : dialogRef.current.showModal();
-  }
-
-  const submit2 = () => {
-    confirmAlert({
-      customUI: ({ onClose }) => {
-        return (
-          <div className={styles.testttttt}>
-            <p>Are you sure?</p>
-
-            <button className={styles.inputBoxButton} onClick={onClose}>
-              no
-            </button>
-            <button
-              className={styles.inputBoxButton}
-              onClick={() => {
-                this.handleClickDelete();
-                onClose();
-              }}
-            >
-              Yes
-            </button>
-          </div>
-        );
-      },
-    });
-  };
-
-  const numContoller = (e, setFunc) => {
+  const numContoller = (e) => {
     if (e.target.value === "09") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "08") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "07") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "06") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "05") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "04") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "03") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "02") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "01") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "00") {
-      setFunc(0);
+      setText(0);
     } else if (e.target.value === "--") {
-      setFunc(300);
+      setText(300);
     } else if (e.target.value > 300) {
-      setFunc(300);
+      setText(300);
     } else if (e.target.value >= 0) {
-      setFunc(e.target.value);
+      setText(e.target.value);
     }
   };
 
@@ -87,8 +48,8 @@ const ParamInput = ({
       title: `${text} cm. Are you sure?`,
       text: "You won't be able to revert this!",
       showCancelButton: true,
-      confirmButtonColor: "rgb(78, 97, 97)",
-      cancelButtonColor: "#rgb(78, 97, 97)",
+      confirmButtonColor: "rgb(3, 197, 197)",
+      cancelButtonColor: "rgb(141, 141, 141)",
       confirmButtonText: "Save",
     }).then((result) => {
       if (result.isConfirmed) {
@@ -118,9 +79,6 @@ const ParamInput = ({
           }}
         />
       </div>
-      {/* <button className={styles.inputBoxButton} onClick={toggleDialog}>
-        ssad
-      </button> */}
 
       <button
         disabled={inputDateVal && !futureORequal(inputDateVal)}
